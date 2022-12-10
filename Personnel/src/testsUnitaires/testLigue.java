519032191661745eaf2aeea114e8b29be0062d0c
Mariecls <marie.ucles@neuf.fr>
2022-12-10 13:10:45 +0100

    testligueMarie

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,4 +1,4 @@
-package testsUnitaires;
+package serialisation;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -22,5 +22,57 @@ class testLigue
 		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
+	}
+	@Test 
+	void getNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("rugby");
+		assertEquals("rugby", ligue.getNom());
+	}
+	
+	@Test
+	void setNom() throws SauvegardeImpossible{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		ligue.setNom("Bowling");
+		assertEquals("Bowling", ligue.getNom());
+	}
+	@Test 
+	void getAdministrateur() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe administrateur = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
+		ligue.setAdministrateur(administrateur);
+		assertEquals(administrateur, ligue.getAdministrateur());
+	}
+	@Test
+	void setAdministrateur() throws SauvegardeImpossible{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe administrateur = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
+		ligue.setAdministrateur(administrateur);
+		assertEquals(administrateur, ligue.getAdministrateur());
+		
+		
+	}
+	@Test
+	void remove() throws SauvegardeImpossible{
+		Ligue ligue = gestionPersonnel.addLigue("ligue");
+		Employe employe1 = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+		Employe employe2 = ligue.addEmploye("Ucles", "Marie", "marie.ucles@gmail.com", "poiuyh");
+		ligue.remove();
+		assertEquals(false, gestionPersonnel.getLigues().equals(ligue));
+	}
+	@Test
+	void compareTo() throws SauvegardeImpossible{
+		Ligue rugby = gestionPersonnel.addLigue("rugby");
+		Ligue football= gestionPersonnel.addLigue("football");
+		assertEquals(24, rugby.compareTo(rugby));
+		assertEquals(15, rugby.compareTo(football));
+		}
+	
+	@Test 
+	void testToString() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("rugby");
+		assertEquals("rugby", ligue.toString());
 	}
 }
