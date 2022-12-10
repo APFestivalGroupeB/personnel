a54aa3b11b3bfd26f1714d4e6de004feb4570a20
Hschoeffler <47255902+HadrienSchoeffler@users.noreply.github.com>
2022-12-10 17:07:52 +0100

    Update testLigue.java

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,4 +1,4 @@
-package serialisation;
+package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -23,61 +23,6 @@ class testLigue
 		Employe employe = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
 		assertEquals(employe, ligue.getEmployes().first());
 	}
-<<<<<<< HEAD
-	@Test 
-	void getNom() throws SauvegardeImpossible
-	{
-		Ligue ligue = gestionPersonnel.addLigue("rugby");
-		assertEquals("rugby", ligue.getNom());
-	}
-	
-	@Test
-	void setNom() throws SauvegardeImpossible{
-		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
-		ligue.setNom("Bowling");
-		assertEquals("Bowling", ligue.getNom());
-	}
-	@Test 
-	void getAdministrateur() throws SauvegardeImpossible
-	{
-		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
-		Employe administrateur = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
-		ligue.setAdministrateur(administrateur);
-		assertEquals(administrateur, ligue.getAdministrateur());
-	}
-	@Test
-	void setAdministrateur() throws SauvegardeImpossible{
-		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
-		Employe administrateur = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty"); 
-		ligue.setAdministrateur(administrateur);
-		assertEquals(administrateur, ligue.getAdministrateur());
-		
-		
-	}
-	@Test
-	void remove() throws SauvegardeImpossible{
-		Ligue ligue = gestionPersonnel.addLigue("ligue");
-		Employe employe1 = ligue.addEmploye("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
-		Employe employe2 = ligue.addEmploye("Ucles", "Marie", "marie.ucles@gmail.com", "poiuyh");
-		ligue.remove();
-		assertEquals(false, gestionPersonnel.getLigues().equals(ligue));
-	}
-	@Test
-	void compareTo() throws SauvegardeImpossible{
-		Ligue rugby = gestionPersonnel.addLigue("rugby");
-		Ligue football= gestionPersonnel.addLigue("football");
-		assertEquals(24, rugby.compareTo(rugby));
-		assertEquals(15, rugby.compareTo(football));
-		}
-	
-	@Test 
-	void testToString() throws SauvegardeImpossible
-	{
-		Ligue ligue = gestionPersonnel.addLigue("rugby");
-		assertEquals("rugby", ligue.toString());
-	}
-}
-=======
 	@Test
 	void getNom() throws SauvegardeImpossible
 	{
@@ -123,7 +68,7 @@ class testLigue
 		Employe employe = ligue.addEmploye ("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
 		Employe employe2 = ligue.addEmploye ("Michel", "Jean", "j.michel@gmail.com", "azerty");
 		employe.remove();
-		assertNotEquals(employe,ligue.getEmployes());
+		assertEquals(employe,ligue.getEmployes());
 	}
 	@Test
 	void removeligue() throws SauvegardeImpossible
@@ -133,5 +78,17 @@ class testLigue
 		ligue.remove();
 		assertNotEquals(ligue, gestionPersonnel.getLigues());
 	}
+	@Test
+	void compareTo() throws SauvegardeImpossible
+	{
+		Ligue ligue1 = gestionPersonnel.addLigue("Bowling");
+		Ligue ligue2 = gestionPersonnel.addLigue("Flechettes");
+		assertEquals(-4, ligue1.compareTo(ligue2));
+	}
+	@Test 
+	void ToString() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertEquals("Fléchettes", ligue.toString());
+	}
 }
->>>>>>> 6fd2c5ed854f08e6a95a2c6bf290f3404c62dea4
