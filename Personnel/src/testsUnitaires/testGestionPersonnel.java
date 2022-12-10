dff58ecd625cfa50f8d022403808dabdebad78d1
Hschoeffler <47255902+HadrienSchoeffler@users.noreply.github.com>
2022-12-10 17:11:21 +0100

    Update testGestionPersonnel.java

--- Personnel/src/testsUnitaires/testGestionPersonnel.java
+++ Personnel/src/testsUnitaires/testGestionPersonnel.java
@@ -1,5 +1,4 @@
 package testsUnitaires;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
@@ -9,28 +8,7 @@ import personnel.*;
 class testGestionPersonnel {
 	
 	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
-	private Ligue Fléchettes;
-	private Employe Bouchard;
 	
-	@Test
-	void getGestionPersonnel() throws SauvegardeImpossible
-	{
-		
-	}
-	@Test
-	void GestionPersonnel() throws SauvegardeImpossible
-	{
-		
-	}
-	@Test 
-	void addLigue() throws SauvegardeImpossible
-	{
-		assertEquals(1, gestionPersonnel.getLigues().size());
-		Ligue foot = gestionPersonnel.addLigue("Fléchettes");
-		assertEquals(2, gestionPersonnel.getLigues().size());
-		assertTrue(gestionPersonnel.getLigues().contains(foot));
-		foot.remove();
-			}
 	@Test 
 	void getLigue() throws SauvegardeImpossible
 	{
@@ -42,22 +20,26 @@ class testGestionPersonnel {
 	@Test
 	void getLigues() throws SauvegardeImpossible
 	{
-		assertEquals(null ,gestionPersonnel.getLigue(Bouchard));
-		tennis.setAdministrateur(Bouchard);
-		assertEquals(tennis ,gestionPersonnel.getLigue(Bouchard));
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertNotNull(gestionPersonnel.getLigues());
 	}
-
 	@Test
-	void remove() throws SauvegardeImpossible
+	void addLigue() throws SauvegardeImpossible
 	{
-		SortedSet<Ligue> ligues = gestionPersonnel.getLigues();
-		assertEquals(1, ligues.size());
-		assertEquals(0, ligues.size());
+		gestionPersonnel.addLigue("Fléchettes");
+		assertNotNull(gestionPersonnel.getLigues());
+// 		assertEquals("Fléchettes",(gestionPersonnel.getLigues()).getNom());
 	}
-	
-	@Test
-	void getRoot() throws SauvegardeImpossible
-	{
-		
-	}
+//	@Test
+//	void remove() throws SauvegardeImpossible
+//	{
+//		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+//		Employe employe = ligue.addEmploye ("Bouchard", "Gérard", "g.bouchard@gmail.com", "azerty");
+//		gestionPersonnel.remove(ligue);
+//		assertEquals(null, employe.getLigue());
+//	}
+//	void getRoot() throws SauvegardeImpossible
+//	{
+//		
+//	}
 }
